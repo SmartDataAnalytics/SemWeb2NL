f78faee10f71b6571102c05c2488c5b9904165f4
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-10-07 08:47:22 +0200

    Some utils.

---
+++ assess/src/main/java/org/aksw/assessment/util/NlpUtils.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.aksw.assessment.util;
+
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+
+/**
+ * @author Lorenz Buehmann
+ *
+ */
+public class NlpUtils {
+	
+	public static void main(String[] args) throws Exception {
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
+	    Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	    
+	    // read some text in the text variable
+	    String text = "Whose birthplace is London?";
+	    
+	    // create an empty Annotation just with the given text
+	    Annotation document = new Annotation(text);
+	    
+	    // run all Annotators on this text
+	    pipeline.annotate(document);
+	    
+	    // these are all the sentences in this document
+	    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+	    
+	    for(CoreMap sentence: sentences) {
+	      // traversing the words in the current sentence
+	      // a CoreLabel is a CoreMap with additional token-specific methods
+	      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+	        // this is the text of the token
+	        String word = token.get(TextAnnotation.class);
+	        // this is the POS tag of the token
+	        String pos = token.get(PartOfSpeechAnnotation.class);
+	        // this is the NER label of the token
+	        String ne = token.get(NamedEntityTagAnnotation.class);
+	        System.out.println(token);
+	      }
+
+	      // this is the parse tree of the current sentence
+	      Tree tree = sentence.get(TreeAnnotation.class);
+	      tree.indentedListPrint();
+
+	      // this is the Stanford dependency graph of the current sentence
+	      SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+	    }
+	}
+
+}
---
+++ assess/src/main/java/org/aksw/assessment/util/SPARQLQueryUtils.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package org.aksw.assessment.util;
+
+import org.aksw.sparqltools.util.SPARQLEndpointType;
+
+import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.SortCondition;
+import com.hp.hpl.jena.sparql.core.Var;
+import com.hp.hpl.jena.sparql.expr.E_Function;
+import com.hp.hpl.jena.sparql.expr.ExprAggregator;
+import com.hp.hpl.jena.sparql.expr.ExprList;
+import com.hp.hpl.jena.sparql.expr.aggregate.AggCountVar;
+import com.hp.hpl.jena.sparql.syntax.ElementGroup;
+import com.hp.hpl.jena.sparql.util.ExprUtils;
+
+/**
+ * @author Lorenz Buehmann
+ *
+ */
+public class SPARQLQueryUtils {
+	
+	/**
+	 * Defines the default SPARQL prefixes for rdf, rdfs and owl mostly used in SPARQL queries.
+	 */
+	public static final String DEFAULT_PREFIXES = 
+			"PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+			+ "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\n"
+			+ "PREFIX owl:<http://www.w3.org/2002/07/owl#>\n";
+	
+	private static final String INCOMING_LINK_VAR_NAME = "?incoming";
+	
+	/**
+     * For some {@link SPARQLEndpointType SPARQL endpoint types} we need to add an additional constraint to
+     * determine the ranking information based on incoming relations of the entities.
+     * <p>
+     * Currently this done for {@link SPARQLEndpointType#ARQ ARQ} and {@link SPARQLEndpointType#LARQ LARQ}.
+     * 
+     * @param endpointType
+     *            the endpoint type
+     * @param queryString
+     *            the SPARQL query string to add the ranking constraint
+     * @param rootVarName
+     *            the variable name used to select entities
+     */
+    public static void addRanking(SPARQLEndpointType endpointType,
+                                              final Query query,
+                                              final Var rootVar) {
+    	addRankingConstraints(endpointType, query, rootVar);
+    	addRankingOrder(endpointType, query, rootVar);
+    }
+	
+	/**
+     * For some {@link SPARQLEndpointType SPARQL endpoint types} we need to add an additional constraint to
+     * determine the ranking information based on incoming relations to the Entities.
+     * <p>
+     * Currently this done for {@link SPARQLEndpointType#ARQ ARQ} and {@link SPARQLEndpointType#LARQ LARQ}.
+     * 
+     * @param endpointType
+     *            the endpoint type
+     * @param queryString
+     *            the SPARQL query string to add the ranking constraint
+     * @param rootVarName
+     *            the variable name used to select entities
+     */
+    public static void addRankingConstraints(SPARQLEndpointType endpointType,
+                                              final Query query,
+                                              final Var rootVar) {
+    	// for Virtuoso we do not need to count incoming links, because it
+        // has a page rank like feature that we can use to rank entities!
+        // all others do not support sorting
+    	if(endpointType == SPARQLEndpointType.Virtuoso){
+    		
+    	} else {
+    		ElementGroup whereClause = (ElementGroup) query.getQueryPattern();
+    		whereClause.addTriplePattern(Triple.create(
+    				NodeFactory.createVariable(INCOMING_LINK_VAR_NAME),
+    				NodeFactory.createVariable("p_in"),
+    				rootVar));
+    	}
+    }
+
+    /**
+     * @param endpointType
+     * @param queryString
+     */
+    public static void addRankingOrder(SPARQLEndpointType endpointType,
+    		final Query query,
+            final Var rootVar) {
+        if (endpointType == SPARQLEndpointType.Virtuoso) {
+        	query.addOrderBy(
+        			new SortCondition(
+        					new E_Function("LONG::IRI_RANK", new ExprList(ExprUtils.nodeToExpr(rootVar))),
+        					Query.ORDER_DESCENDING));
+        } else {
+            // TODO: COUNT is not part of the SPARQL 1.0 specification!
+            // see http://www.w3.org/2009/sparql/wiki/Feature:AggregateFunctions
+        	query.addOrderBy(
+        			new SortCondition(
+        					new ExprAggregator(
+        							rootVar, 
+        							new AggCountVar(ExprUtils.nodeToExpr(rootVar))), 
+        					Query.ORDER_DESCENDING));
+        } 
+    }
+
+}
