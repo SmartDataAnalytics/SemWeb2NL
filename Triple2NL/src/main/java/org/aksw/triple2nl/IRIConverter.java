321d860ebca300fa025b0ba1058271043df86e42
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-10-21 06:56:10 +0200

    Code cleanup.

--- Triple2NL/src/main/java/org/aksw/triple2nl/IRIConverter.java
+++ Triple2NL/src/main/java/org/aksw/triple2nl/IRIConverter.java
@@ -4,10 +4,24 @@
 package org.aksw.triple2nl;
 
 /**
+ * Converts IRIs into natural language.
  * @author Lorenz Buehmann
  *
  */
 public interface IRIConverter {
+	
+	/**
+	 * Convert the IRI into natural language.
+	 * @param iri the IRI
+	 * @return a natural language representation
+	 */
 	String convert(String iri);
-	String convert(String iri, boolean dereferenceIRI);
+	
+	/**
+	 * Convert the IRI into a natural language.
+	 * @param iri the IRI to convert
+	 * @param useDereferencing whether to try Linked Data dereferencing of the IRI
+	 * @return a natural language representation
+	 */
+	String convert(String iri, boolean useDereferencing);
 }
